a9e31ff83058107b6fb5d156bb83a5a86182b612
Nathan Chiu <nathan.chiu94@gmail.com>
2021-05-30 16:03:49 -0700

    Change config items.

--- src/ygo_bot/config.py
+++ src/ygo_bot/config.py
@@ -1,2 +1,2 @@
 # Make sure not to add this into git changelogs, or else your token will be revealed to the world!
-disc_token = 'YOUR_TOKEN_HERE'
+disc_token = 'ODMwMzE2ODIxODY5MzYzMjEy.YHE6zA.CQRZ4NbE-f6X86hXXKZIoV3Dm4I'
---
+++ src/ygo_bot/config_example.py
@@ -0,0 +1,2 @@
+# Change this file's name to "config.py", and fill in the disc_token.
+disc_token = 'EXAMPLE'
